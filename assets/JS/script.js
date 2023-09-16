0aabca5c84ea752af5e6edf397096187aa188dc9
dmod129 <derek.modugno@gmail.com>
2023-09-16 16:49:52 -0500

    styled card divs

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,14 @@
-var APIkey = "6636d0f4895a55da96fb3ffcd29a6dd1";
-
-var city;
-
+var APIKey = "6636d0f4895a55da96fb3ffcd29a6dd1";
+var city = Austin;
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-fetch(queryURL);
+// Fetch weather data for city
+function getWeather(query); {
+    queryURL;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+}
+
+http://api.openweathermap.org/data/2.5/weather?q=Austin&appid=6636d0f4895a55da96fb3ffcd29a6dd1
