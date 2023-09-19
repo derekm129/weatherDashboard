990051853375dba143b4df4e66b5bc7a4f35741b
dmod129 <derek.modugno@gmail.com>
2023-09-18 21:26:18 -0500

    added forecastWeather function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,12 +1,12 @@
 var APIKey = "6636d0f4895a55da96fb3ffcd29a6dd1";
-var searchButton = document.getElementById("searchBtn")
+var searchButton = document.getElementById("searchBtn");
 
 
 // const city = document.getElementById("cityInput");
 // var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
 // Search Button
-searchButton.addEventListener('click', fetchWeather);
+searchButton.addEventListener('click', fetchWeather, forecastWeather);
 
 // Get city name
 // function getCity() {
@@ -16,12 +16,47 @@ searchButton.addEventListener('click', fetchWeather);
 // }
 
 // Fetch weather data for city
-
+// Current Weather
 function fetchWeather(query) {
     let city = document.getElementById("cityInput").value;
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
     document.getElementById("cityName").innerHTML = city;
     console.log("button clicked");
+
+    // Current Weather
+    fetch(queryURL)
+        .then(function (response) {
+            console.log(response.json);
+            return response.json();  
+        })
+        .then(function (data) {
+            
+            let date = new Date(data.dt);
+            let temp = (data.main.temp);
+            let humidity = (data.main.humidity);
+            let windSpeed = (data.wind.speed);
+            let weatherIcon = (data.weather.icon);
+
+            document.getElementById("date1").innerHTML = date;
+            document.getElementById("temp1").innerText = temp.toString();
+            document.getElementById("humidity1").innerText = humidity.toString();
+            document.getElementById("windSpeed1").innerText = windSpeed.toString();
+
+            // icon1 = document.getElementById("icon1");
+            // // Icons
+            // if(data.weather.main == "clear") {
+            //     icon1.src = "http://openweathermap.org/img/w/10d.png";
+            // }
+            console.log(data);
+        forecastWeather(data.coord.lat, data.coord.lon);
+        }); 
+      
+    };
+
+    // 5 Day forecast
+function forecastWeather(latitude, longitude) {
+    let city = document.getElementById("cityInput").value;
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" +latitude + "&lon=" + longitude + "&appid=" + APIKey + "&units=imperial";
 
     fetch(queryURL)
         .then(function (response) {
@@ -29,24 +64,15 @@ function fetchWeather(query) {
             return response.json();  
         })
         .then(function (data) {
-            let date = new Date(data.dt);
-            let maxTemp = (data.main.temp_max);
-            let minTemp = (data.main.temp_min);
-            let humidity = (data.main.humidity);
-            let windSpeed = (data.wind.speed);
-
-            console.log(maxTemp);
-
-            document.getElementById("date1").innerHTML = date;
-            console.log(document.getElementById("maxTemp1").value);
-            document.getElementById("maxTemp1").innerText = maxTemp.toString();
-            document.getElementById("minTemp1").innerText = minTemp.toString();
-            document.getElementById("humidity1").innerText = humidity.toString();
-            document.getElementById("windSpeed1").innerText = windSpeed.toString();
-            console.log(data)
-        }); 
-    }
-    
+            console.log(data);
+            let date = new Date(data.list.dt);
+            let temp = (data.list.main.temp);
+            let humidity = (data.list.main.humidity);
+            let windSpeed = (data.list.wind.speed);
+            let weatherIcon = (data.weather.icon);
+            
+        })
+};
 
 
 
